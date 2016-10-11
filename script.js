c23a2435d2f47d0b969d8fb26279608aa9754253
Dan Stineback <dstineback@gmail.com>
2016-10-10 20:39:07 -0700

    added html and js

---
+++ script.js
@@ -0,0 +1,10 @@
+function lib() {
+  var story = document.getElementById('story');
+  var name = document.getElementById('name').value;
+  var adjetive = document.getElementById('adjetive').value;
+  var noun = document.getElementById('noun').value;
+  story.innerHTML = name + " " + noun + " " + adjetive + " " + "love dogs!";
+
+}
+var button = document.getElementById('button');
+button.addEventlistner('click', lib);
